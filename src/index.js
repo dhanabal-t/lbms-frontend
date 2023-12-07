a6920bfbbdc686fb1652986c14562428bb20c4ec
Dhanabal T <86637479+dhanabal-t@users.noreply.github.com>
2023-12-07 12:04:57 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { AuthContextProvider } from './Context/AuthContext';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+serviceWorkerRegistration.register();
+
+reportWebVitals();
+
